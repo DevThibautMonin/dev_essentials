e7c67a5479a5d2d417bee6e62e52072838050e30
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-05-18 17:52:43 +0200

    Create gaps.dart
    creating compile time gaps

---
+++ lib/widgets/gaps.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Gaps {
+  static const gap5H = SizedBox(height: 5);
+  static const gap10H = SizedBox(height: 10);
+  static const gap15H = SizedBox(height: 15);
+
+  static const gap5W = SizedBox(width: 5);
+  static const gap10W = SizedBox(width: 10);
+  static const gap15W = SizedBox(width: 15);
+}
