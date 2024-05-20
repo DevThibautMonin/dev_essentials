9a16276deb0b50e71a679c0acb8d2bed0e18dcb0
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-05-20 21:07:38 +0200

    Update tool_screen.dart

--- lib/presentation/tool_screen.dart
+++ lib/presentation/tool_screen.dart
@@ -1,16 +1,17 @@
+import 'package:dev_essentials/presentation/char_counter/char_counter_screen.dart';
 import 'package:dev_essentials/presentation/json_formatter/json_formatter_screen.dart';
 import 'package:dev_essentials/presentation/toolbar.dart';
+import 'package:dev_essentials/widgets/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ToolScreen extends StatelessWidget {
   const ToolScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        Flexible(
+        const Flexible(
           flex: 2,
           child: Column(
             children: [
@@ -20,10 +21,14 @@ class ToolScreen extends StatelessWidget {
         ),
         Flexible(
           flex: 8,
-          child: Column(
-            children: [
-              JsonFormatterScreen(),
-            ],
+          child: Container(
+            color: CustomColors.primaryColor,
+            child: const Column(
+              children: [
+                // JsonFormatterScreen(),
+                CharCounterScreen()
+              ],
+            ),
           ),
         ),
       ],
