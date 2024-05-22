457947634df1cb7f4717880604dc51ab3d5e45fb
Thibaut Monin <device.thibaut.monin@gmail.com>
2024-05-22 21:33:41 +0200

    Update tools.dart
    add Text Generator tool

--- lib/presentation/tools.dart
+++ lib/presentation/tools.dart
@@ -1,4 +1,5 @@
 import 'package:dev_essentials/presentation/char_counter/char_counter_screen.dart';
+import 'package:dev_essentials/presentation/text_generator/text_generator_screen.dart';
 import 'package:dev_essentials/presentation/tools_screen.dart';
 import 'package:dev_essentials/presentation/json_formatter/json_formatter_screen.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ List<Tool> tools = [
   const Tool(title: "All Tools", icon: Icons.list, widget: ToolsScreen()),
   const Tool(title: "JSON Formatter", icon: Icons.code, widget: JsonFormatterScreen()),
   const Tool(title: "Char Counter", icon: Icons.onetwothree, widget: CharCounterScreen()),
+  const Tool(title: "Text Generator", icon: Icons.text_increase, widget: TextGeneratorScreen()),
 ];
 
 class Tool {
